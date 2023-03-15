92ef1e9b1b2b37c00c6891dd995605b1b5dfec3b
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-03-15 08:30:07 +0100

    Added Files

---
+++ Tile.js
@@ -0,0 +1,23 @@
+/* Psuedokode
+class Tile {
+    Load Images and place into the array Tiles[]
+}
+
+Draw Images {
+    Draw Images on Screen()
+    Distance between pictures = 5px
+}
+
+Remove last Tile {
+    Remove last element of Tiles[]
+    Push empty square to Tiles[]
+}
+
+Tiles Mix {
+    Tile position = random Position on Board
+}
+*/
+
+class Tile {
+
+}
